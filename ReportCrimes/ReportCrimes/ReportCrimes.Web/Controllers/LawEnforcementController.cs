752e4d7bc73b806b60ff34e623edc124b0e37f9d
Mateusz GABRYŚ <mateuszigabrys@gmail.com>
2021-07-05 18:33:15 +0200

    Add CRUD VIEW for lawenforcementapi

--- ReportCrimes/ReportCrimes/ReportCrimes.Web/Controllers/LawEnforcementController.cs
+++ ReportCrimes/ReportCrimes/ReportCrimes.Web/Controllers/LawEnforcementController.cs
@@ -26,5 +26,75 @@ namespace ReportCrimes.Web.Controllers
             }
             return View(list);
         }
+
+        public async Task<IActionResult> LawEnforcementCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> LawEnforcementCreate(LawEnforcementDto law)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _lawEnforcementService.Create<ResponseDto>(law);
+                if (response != null && response.IsSucces)
+                {
+                    return RedirectToAction(nameof(LawEnforcementIndex));
+                }
+            }
+            return View(law);
+        }
+
+        public async Task<IActionResult> LawEnforcementEdit(int id)
+        {
+            var response = await _lawEnforcementService.GetSingle<ResponseDto>(id);
+            if (response != null && response.IsSucces)
+            {
+                LawEnforcementDto model = JsonConvert.DeserializeObject<LawEnforcementDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> LawEnforcementEdit(LawEnforcementDto law)
+        {
+            if (ModelState.IsValid)
+            {
+                var cos = law;
+                var response = await _lawEnforcementService.Update<ResponseDto>(law);
+                
+                if (response != null && response.IsSucces)
+                {
+                    return RedirectToAction(nameof(LawEnforcementIndex));
+                }
+            }
+            return View(law);
+        }
+
+        public async Task<IActionResult> LawEnforcementDelete(int id)
+        {
+            var response = await _lawEnforcementService.GetSingle<ResponseDto>(id);
+            if (response != null && response.IsSucces)
+            {
+                LawEnforcementDto model = JsonConvert.DeserializeObject<LawEnforcementDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> LawEnforcementDelete(LawEnforcementDto law)
+        {
+            if (ModelState.IsValid)
+            {
+                var cos = law;
+                var response = await _lawEnforcementService.Delete<ResponseDto>(law.LawEnforcementId);
+
+                if (response.IsSucces)
+                {
+                    return RedirectToAction(nameof(LawEnforcementIndex));
+                }
+            }
+            return View(law);
+        }
     }
 }
---
+++ ReportCrimes/ReportCrimes/ReportCrimes.Web/Models/LawEnforcementEditDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportCrimes.Web.Models
+{
+    public class LawEnforcementEditDto
+    {
+        public string RankOfLawEnforcement { get; set; }
+    }
+}
